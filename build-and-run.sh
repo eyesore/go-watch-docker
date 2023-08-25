b110561a511d612d211ebd44cd4884b1450919d7
Trey Jones <trey@cortexdigitalinc.com>
2023-08-25 11:14:45 -0400

    prevent execution of old application on build failure

--- build-and-run.sh
+++ build-and-run.sh
@@ -5,6 +5,11 @@ PASSED_ARGS="${@}"
 go get
 go build -buildvcs=false -o /go/bin/app
 
+if [ $? -ne 0 ]; then
+	"build failed - halting"
+	exit 1
+fi
+
 RUN_CMD="/go/bin/app ${PASSED_ARGS}"
 echo "run cmd: ${RUN_CMD}"
 ${RUN_CMD} &
